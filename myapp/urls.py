62a409d318881c6667cf59806dfdfb19a3815980
Chetan Sanjay Shivade <chetanshivade34@gmail.com>
2023-08-27 20:34:30 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+urlpatterns = [
+    
+    path('hello',views.hello_worls),
+    path('',views.homePage),
+    path('url/task' or 'url',views.taskPage),
+    path('url',views.taskPage),
+] 
